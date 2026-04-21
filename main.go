4c9f462dd9d3c61b37c7f11a76cb8cbfa202de47
gappy <szakosbalazs2007@gmail.com>
2026-04-21 09:51:33 +0200

    fix main.go and fix provider address

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"log"
 
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+
+	"github.com/gappylul/terraform-provider-flagd/internal/provider"
+)
+
+var version = "dev"
+
+func main() {
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "Enable debug mode for provider development")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "gappylul/flagd",
+		Debug:   debug,
+	}
+
+	if err := providerserver.Serve(context.Background(), provider.New(version), opts); err != nil {
+		log.Fatal(err)
+	}
 }
